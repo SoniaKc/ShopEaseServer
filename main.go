f7e6286422506de9d71750848753b8dbad1ef10c
Sonia Kechaou <soniakcha@gmail.com>
2025-05-20 15:50:10 +0200

    N

--- main.go
+++ main.go
@@ -1,26 +1,27 @@
 package main
 
 import (
-    "log"
-    "os"
-    "shop-ease-server/internal/routes"
+	"log"
+	"os"
+	"shop-ease-server/internal/routes"
+	"shop-ease-server/internal/storage"
 )
 
 func main() {
 
-    /*if err := storage.InitPostgres(); err != nil {
-        log.Fatal("PostGre init failed:", err)
-    }*/
+	if err := storage.InitPostgres(); err != nil {
+		log.Fatal("PostGre init failed:", err)
+	}
 
-    router := routes.SetupRouter() // Initialisation du routeur
+	router := routes.SetupRouter() // Initialisation du routeur
 
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8090"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8090"
+	}
 
-    if err := router.Run(":" + port); err != nil {
-        log.Panicf("error: %s", err)
-    }
+	if err := router.Run(":" + port); err != nil {
+		log.Panicf("error: %s", err)
+	}
 
 }
