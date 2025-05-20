fc1914f7804efe5ce298c1b174d18729b96ef21c
Sonia Kechaou <soniakcha@gmail.com>
2025-05-20 15:45:25 +0200

    N

--- internal/storage/user.go
+++ internal/storage/user.go
@@ -1,21 +1,26 @@
 package storage
 
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+)
 
 func AddUser(firstName string, lastName string) error {
 	var count int
-    err := DB.QueryRow("SELECT * FROM users WHERE firstName = $1 AND lastname = $2", firstName, lastName).Scan(&count)
+	err := DB.QueryRow("SELECT * FROM users WHERE firstName = $1 AND lastname = $2", firstName, lastName).Scan(&count)
 
-    if err == sql.ErrNoRows {
-        _, errInsert := DB.Exec("INSERT INTO users (firstName, lastName) VALUES ($1, $2)", firstName, lastName)
-        return errInsert
-    }
+	if err == sql.ErrNoRows {
+		_, errInsert := DB.Exec("INSERT INTO users (firstName, lastName) VALUES ($1, $2)", firstName, lastName)
+		return errInsert
+	}
 
-    if err != nil {
-        return fmt.Errorf("error while checking users: %v", err)
-    }
-    if count > 0 {
-        return errors.New("user already exist")
-    }
-    _, errInsert := DB.Exec("INSERT INTO users (firstName, lastName) VALUES ($1, $2)", firstName, lastName)
-    return errInsert
+	if err != nil {
+		return fmt.Errorf("error while checking users: %v", err)
+	}
+	if count > 0 {
+		return errors.New("user already exist")
+	}
+	_, errInsert := DB.Exec("INSERT INTO users (firstName, lastName) VALUES ($1, $2)", firstName, lastName)
+	return errInsert
 }
